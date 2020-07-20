6f31859cd90f0436cb66864a2ef12eea256cf30e
rijun <richard.jung001@stud.fh-dortmund.de>
2020-07-20 15:32:02 +0200

    Fix path issue

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import configparser
+import os
 
 
 class Config:
     def __init__(self):
         c = configparser.ConfigParser()
-        c.read("config.ini")
+        config_file = os.path.join(os.path.dirname(__file__), "config.ini")
+        c.read(config_file)
         self.sql_user = c['sql']['user']
         self.sql_pass = c['sql']['passwd']
         self.sql_db = c['sql']['db']
