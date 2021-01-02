fe32a8193dc89246bbb6647f64f319c6d061cdb2
rijun <richard.jung001@stud.fh-dortmund.de>
2021-01-02 15:32:36 +0100

    Add display news unit test

--- display_news/parsers/display_news.py
+++ display_news/parsers/display_news.py
@@ -8,15 +8,18 @@ def parse(data: BeautifulSoup) -> list:
         return []
 
     news = []
-    news_title = data.find(class_="newsHeadline")  # Find first news headline
+    news_title = data.find(class_='newsHeadline')  # Find first news headline
     while news_title is not None:  # Search for content until every headline is processed
         title = news_title.string
-        data = news_title.find_next(class_="indent").string  # Next sibling after headline stores the news content
-        news_hash = hashlib.md5((title + data).encode('utf-8')).hexdigest()
+        title = ' '.join(title.split())
+        # Next sibling after headline stores the news content
+        content = news_title.find_next(class_='indent').string
+        content = ' '.join(content.split())
+        news_hash = hashlib.md5((title + content).encode('utf-8')).hexdigest()
         news.append({
             'hash': news_hash,
             'title': title,
-            'data': data
+            'content': content
         })
         news_title = news_title.find_next(class_="newsHeadline")  # Find next headline
 
--- tests/test_display_news.py
+++ tests/test_display_news.py
@@ -1,13 +1,21 @@
+import json
 from unittest import TestCase
 
 from bs4 import BeautifulSoup
 
-from display_news import __main__
+from display_news.parsers import display_news
 
 
 class TestDisplayNews(TestCase):
-    def test_parse_display_news(self):
-        with open("display_news.html", 'r') as f:
-            data = BeautifulSoup(f.readlines(), "html.parser")
-        print(__main__.parse_display_news(data))
-        self.fail()
+    def setUp(self) -> None:
+        with open("files/display_news.html", 'r') as f:
+            self.file_data = BeautifulSoup(f, 'html.parser')
+
+    def test_parser(self):
+        data = display_news.parse(self.file_data)
+        with open("files/display_news.json", 'r') as f:
+            test_data = json.load(f)
+        self.assertEqual(len(data), len(test_data))
+        for i, d in enumerate(data):
+            self.assertEqual(test_data[i]['title'], d['title'])
+            self.assertEqual(test_data[i]['content'], d['content'])
